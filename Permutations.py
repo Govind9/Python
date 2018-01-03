9d3590b48087ca8bf6bba313441503704b06d61b
Govind Sharma <dnivogamrahs@gmail.com>
2018-01-03 16:31:43 +0530

    Update Permutations.py

--- Permutations.py
+++ Permutations.py
@@ -1,9 +1,7 @@
+all_permutations = set()
 def permute (done_str, rem_str):
-    global all_permutations
     if (len(rem_str) == 0):
         all_permutations.add(done_str)
     for i in range(len(rem_str)):
         permute (done_str + rem_str[i], rem_str[:i] + rem_str[i + 1:])
-
-all_permutations = set()
 permute ("", "abcda")
